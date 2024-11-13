befc3e8e0bb61d876f96b2a11551087319f2de80
Retina1 <84415148+Retina1@users.noreply.github.com>
2024-11-12 20:53:49 -0500

    updated planningDoc

--- main.py
+++ main.py
@@ -2,18 +2,74 @@
 # Last Updated: 11-11
 # Description: Holds main function for EE 551 Project 2.
 
+import pygame
+
 # classes to handle player/inventory, map, minimap, enemies?
 
+# file of functions for movement, tie together player (position in class) and map (coordinates in class)
+# combat functions (use elements of enemy classes to determine required dodge?)
+
 def main():
-    runningFlag = True
+    # pygame setup (basic structural code borrowed from docs)
+    pygame.init()
+    screen = pygame.display.set_mode((600, 400))
+    clock = pygame.time.Clock()
+    running = True
 
-    while runningFlag:
-        # Call logic to run the game (other functions)
-        # quit button to allow ending
-        runningFlag = False
+    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-    while not runningFlag:
-        end()
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill("purple")
+
+        # RENDER YOUR GAME HERE
+        # draw squares of different colors in different locations to make ui elements
+
+        pygame.draw.circle(screen, "black", player_pos, 40)
+
+        BUTTON_SIZE = 48
+        pygame.draw.rect(screen, "blue", pygame.Rect(400, 250, BUTTON_SIZE, BUTTON_SIZE))
+        pygame.draw.rect(screen, "blue", pygame.Rect(450, 250, BUTTON_SIZE, BUTTON_SIZE))
+        pygame.draw.rect(screen, "blue", pygame.Rect(500, 250, BUTTON_SIZE, BUTTON_SIZE))
+        pygame.draw.rect(screen, "blue", pygame.Rect(400, 300, BUTTON_SIZE, BUTTON_SIZE))
+        pygame.draw.rect(screen, "blue", pygame.Rect(450, 300, BUTTON_SIZE, BUTTON_SIZE))
+        pygame.draw.rect(screen, "blue", pygame.Rect(500, 300, BUTTON_SIZE, BUTTON_SIZE))
+
+        VIEWPORT_XSIZE = 280
+        VIEWPORT_YSIZE = 240
+        pygame.draw.rect(screen, "green", pygame.Rect(60, 60, VIEWPORT_XSIZE, VIEWPORT_YSIZE))
+
+        MINIMAP_XSIZE = 160
+        MINIMAP_YSIZE = 120
+        pygame.draw.rect(screen, "red", pygame.Rect(395, 60, MINIMAP_XSIZE, MINIMAP_YSIZE))
+
+
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            player_pos.y -= 300 * dt
+        if keys[pygame.K_s]:
+            player_pos.y += 300 * dt
+        if keys[pygame.K_a]:
+            player_pos.x -= 300 * dt
+        if keys[pygame.K_d]:
+            player_pos.x += 300 * dt
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        # limits FPS to 60
+        # dt is delta time in seconds since last frame, used for framerate-
+        # independent physics.
+        dt = clock.tick(60) / 1000
+
+    pygame.quit()
 
 if __name__ == '__main__':
         main()
--- planningDoc.md
+++ planningDoc.md
@@ -1,11 +1,24 @@
-## Possible classes
+## Class concepts
 
 # Player class
-Stores position, inventory, manages movement
+init to handle starting hp and inventory
+Stores position - index in our map grid ['x','y'], inventory, manages movement
+Stats - HP (if hit 0, game ends)
+Inventory list - text file containing item names/data
+item csv containing names/properties - player object only stores id numbers
+lots of basic items, rare cool items with relatively manageable exceptions
+inventory system to popout larger images of items (fun auxilary system)
 
 # Map class
-Stores map makeup, state of gates
+__layout (list of lists, each element is an object related to tile/room type)
+__init__ takes in x, y for size, generates empty map
+PopulateMap() randomly fills in map with rooms, ensures continuity
+Stores map composition, state of gates(?), manages map generation
+Would storing position here make more sense than a player class?
+Classes for tile types?
+set up logic for tracking where we've been (minimap populates as you enter rooms)
+Room object contains room type, isVisted
 
 # Enemy superclass
-Stores general variables for enemies (direction needed to dodge/hit) and have testing logic
+Stores general variables for enemies (direction needed to dodge/hit, maybe position if they're pre-loaded traps instead of random encounters?) and have testing logic
 Subclasses that initialize directions
