644d3560e1ac0dff6d56773b6ad3010a988e7b81
Retina1 <84415148+Retina1@users.noreply.github.com>
2024-11-12 20:51:26 -0500

    added planningDoc.md

---
+++ main.py
@@ -0,0 +1,19 @@
+# Authors: Joshua R., Grant S.
+# Last Updated: 11-11
+# Description: Holds main function for EE 551 Project 2.
+
+# classes to handle player/inventory, map, minimap, enemies?
+
+def main():
+    runningFlag = True
+
+    while runningFlag:
+        # Call logic to run the game (other functions)
+        # quit button to allow ending
+        runningFlag = False
+
+    while not runningFlag:
+        end()
+
+if __name__ == '__main__':
+        main()
---
+++ planningDoc.md
@@ -0,0 +1,11 @@
+## Possible classes
+
+# Player class
+Stores position, inventory, manages movement
+
+# Map class
+Stores map makeup, state of gates
+
+# Enemy superclass
+Stores general variables for enemies (direction needed to dodge/hit) and have testing logic
+Subclasses that initialize directions
